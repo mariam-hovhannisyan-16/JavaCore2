83a0369c35481687e3ef5b6816d9b36a2ccc2454
Mariam Hovhannisyan <mariam-hovhannisyan-16>
2025-10-08 00:49:17 +0400

    Merge remote-tracking branch 'origin/master'

---
+++ src/Homework1/BraceCheckerTest.java
@@ -0,0 +1,8 @@
+package Homework1;
+
+public class BraceCheckerTest {
+    public static void main(String[] args) {
+        BraceChecker bc = new BraceChecker("Hello from [java)");
+        bc.check();
+    }
+}
